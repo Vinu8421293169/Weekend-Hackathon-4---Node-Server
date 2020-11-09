813767ac16b731d6cb3f8e0f7834e690f55d2cd5
Vinod Vyankatesh Gentyal <Vinod.gentyal@gmail.com>
2020-11-09 17:24:59 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,20 +3,19 @@ var http = require("http");
 const httpServer = http.createServer(handleServer);
 
 function handleServer(req, res) {
-  if(req.url==='/welcome'){
-        res.write('Welcome to Dominos!');
-        res.end();
-    }
-
-    if(req.url ==='/contact'){
-        res.write(JSON.stringify({
+    if(req.url==="/welcome")
+    res.end("Welcome to Dominos!");
+    else if(req.url==="/contact"){
+        res.writeHead(200);
+    res.end(JSON.stringify({
             phone: '18602100000',
             email: 'guestcaredominos@jublfood.com'
             }));
-        res.end();
     }
+    else
+    res.writeHead(404)
+    res.end("404 not found");
 }
-
-// server.listen(8081);
+// httpServer.listen(8081);
 
 module.exports = httpServer;
